22d3dd2d0f3c36318bf1cc53a53330c8bfb6968d
Givi363906 <49366026+Givi363906@users.noreply.github.com>
2020-06-30 14:38:39 +0300

    Initial commit

---
+++ app/src/main/java/com/amixenstudio/test/MainActivity.java
@@ -0,0 +1,17 @@
+package com.amixenstudio.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
