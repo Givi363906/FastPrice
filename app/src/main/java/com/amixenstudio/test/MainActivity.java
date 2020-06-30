7530ffc08f3f157512066f47dddf42810bdcf9da
Givi363906 <49366026+Givi363906@users.noreply.github.com>
2020-06-30 22:30:07 +0300

    add python lang

--- app/src/main/java/com/amixenstudio/test/MainActivity.java
+++ app/src/main/java/com/amixenstudio/test/MainActivity.java
@@ -3,15 +3,31 @@ package com.amixenstudio.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.testing);
+
+        if (! Python.isStarted())
+            Python.start(new AndroidPlatform(this));
+
+        Python py = Python.getInstance();
+
+        PyObject py_file = py.getModule("Test"); //name file
+
+        PyObject obj = py_file.callAttr("test"); // def name
+
+        txt.setText(obj.toString());
     }
 }
